bb59567c6dcaf84aa24840618a33bb254ef44197
bbb <111@qq.com>
2022-09-15 22:35:12 +0800

    lodash

--- lodash/baimingsong-lodash.js
+++ lodash/baimingsong-lodash.js
@@ -3,7 +3,7 @@ var baimingsong = {
     var arr1 = []
     var result = []
     var count = 0
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       arr1.push(ary[i])
       count++
       if (count == size) {
@@ -12,7 +12,7 @@ var baimingsong = {
         count = 0
       }
     }
-    if (arr1.length !== 0) {  // 此时 ary.length % size == 剩下的
+    if (arr1.lgth !== 0) {  // 此时 ary.lgth % size == 剩下的
       result.push(arr1)
     }
     return result
@@ -20,7 +20,7 @@ var baimingsong = {
 
   compact: function compact(ary) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (ary[i]) {
         result.push(ary[i])
       }
@@ -31,7 +31,7 @@ var baimingsong = {
   difference: function difference(ary, ...values) {
     var result = []
     values = values.flat()
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (!(values.includes(ary[i]))) {
         result.push(ary[i])
       }
@@ -45,15 +45,15 @@ var baimingsong = {
     if (typeof (iteratee) == 'string') {
       return this.difference(ary, values)
     }
-    for (var i = 0; i < ary1.length; i++) {
+    for (var i = 0; i < ary1.lgth; i++) {
       ary1[i] = iteratee(ary1[i])
 
     }
-    for (var i = 0; i < values.length; i++) {
+    for (var i = 0; i < values.lgth; i++) {
       values[i] = iteratee(values[i])
     }
     var a = this.difference(ary1, values)
-    for (var j = 0; j < ary.length; j++) {
+    for (var j = 0; j < ary.lgth; j++) {
       if (a.includes(iteratee(ary[j])))
         result.push(ary[j])
     }
@@ -62,8 +62,8 @@ var baimingsong = {
 
   differenceWith: function differenceWith(ary, values, comparator) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
-      for (var j = i; j < values.length; j++) {
+    for (var i = 0; i < ary.lgth; i++) {
+      for (var j = i; j < values.lgth; j++) {
         comparator(ary[i], values[j])
       }
     }
@@ -74,13 +74,13 @@ var baimingsong = {
     if (n == 0) {
       return ary
     }
-    if (n >= ary.length) {
+    if (n >= ary.lgth) {
       return []
     }
     if (n == undefined) {
-      return ary.slice(1, ary.length)
+      return ary.slice(1, ary.lgth)
     }
-    return ary.slice(n, ary.length)
+    return ary.slice(n, ary.lgth)
   },
 
 
@@ -88,17 +88,17 @@ var baimingsong = {
     if (n == 0) {
       return ary
     }
-    if (n >= ary.length) {
+    if (n >= ary.lgth) {
       return []
     }
-    return ary.slice(0, ary.length - n)
+    return ary.slice(0, ary.lgth - n)
   },
 
   dropWhile: function dropWhile() {
 
   },
 
-  fill: function fill(ary, value, start = 0, end = ary.length) {
+  fill: function fill(ary, value, start = 0, end = ary.lgth) {
     for (var i = start; i < end; i++) {
       ary[i] = value
     }
@@ -107,7 +107,7 @@ var baimingsong = {
 
   findIndex: function findIndex(array, predicate = identity, fromIndex = 0) {
     if (Array.isArray(predicate)) {
-      for (var i = fromIndex; i < array.length; i++) {
+      for (var i = fromIndex; i < array.lgth; i++) {
         for (var key in array[i]) {
           if (key === predicate[0] && array[i][key] === predicate[1]) {
             return i
@@ -116,7 +116,7 @@ var baimingsong = {
       }
     }
     if (typeof predicate === 'object') {
-      for (var i = fromIndex; i < array.length; i++) {
+      for (var i = fromIndex; i < array.lgth; i++) {
         var flag = true
         for (var key in predicate) {
           if (key in array[i] && predicate[key] === array[i][key]) {
@@ -132,14 +132,14 @@ var baimingsong = {
       }
     }
     if (typeof predicate === 'string') {
-      for (var i = fromIndex; i < array.length; i++) {
+      for (var i = fromIndex; i < array.lgth; i++) {
         if (array[i][predicate] === true) {
           return i
         }
       }
     }
     if (typeof predicate === 'function') {
-      for (var i = fromIndex; i < array.length; i++) {
+      for (var i = fromIndex; i < array.lgth; i++) {
         if (predicate(array[i])) {
           return i
         }
@@ -157,9 +157,9 @@ var baimingsong = {
 
   flatten: function flatten(ary) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (Array.isArray(ary[i])) {
-        for (var j = 0; j < (ary[i]).length; j++) {
+        for (var j = 0; j < (ary[i]).lgth; j++) {
           result.push(ary[i][j])
         }
       } else if (!(Array.isArray(ary[i]))) {
@@ -176,7 +176,7 @@ var baimingsong = {
 
   flattenDepth: function flattenDepth(ary, depth = 1) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (Array.isArray(ary[i]) && depth > 0) {
         result = result.concat(flattenDepth(ary[i], depth - 1))
       } else {
@@ -188,21 +188,21 @@ var baimingsong = {
 
   fromPairs: function fromPairs(pairs) {
     var obj = {}
-    for (var i = 0; i < pairs.length; i++) {
+    for (var i = 0; i < pairs.lgth; i++) {
       obj[pairs[i][0]] = pairs[i][1]
     }
     return obj
   },
 
   head: function head(ary) {
-    if (ary.length == 0) {
+    if (ary.lgth == 0) {
       return undefined
     }
     return ary[0]
   },
 
   indexOf: function indexOf(ary, value, fromIndex = 0) {
-    for (var i = fromIndex; i < ary.length; i++) {
+    for (var i = fromIndex; i < ary.lgth; i++) {
       if (ary[i] == value) {
         return i
       }
@@ -210,11 +210,11 @@ var baimingsong = {
     return -1
   },
   last: function last(ary) {
-    return ary[ary.length - 1]
+    return ary[ary.lgth - 1]
   },
 
 
-  lastIndexOf: function lastIndexOf(ary, value, fromIndex = ary.length - 1) {
+  lastIndexOf: function lastIndexOf(ary, value, fromIndex = ary.lgth - 1) {
     for (var i = fromIndex; i >= 0; i--) {
       if (ary[i] == value) {
         return i
@@ -226,22 +226,21 @@ var baimingsong = {
   initial: function initial(ary) {
     ary.pop()
     return ary
-
   },
 
   join: function join(ary, separator) {
     var result = ''
-    for (var i = 0; i < ary.length - 1; i++) {
+    for (var i = 0; i < ary.lgth - 1; i++) {
       result = result + ary[i] + separator
     }
-    result += ary[ary.length - 1]
+    result += ary[ary.lgth - 1]
     return result
   },
 
 
   pull: function pull(ary, ...values) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (!(values.includes(ary[i]))) {
         result.push(ary[i])
       }
@@ -250,17 +249,17 @@ var baimingsong = {
   },
 
   reverse: function reverse(ary) {
-    var l = Math.floor(ary.length / 2)
+    var l = Math.floor(ary.lgth / 2)
     for (var i = 0; i < l; i++) {
       var temp = ary[i]
-      ary[i] = ary[ary.length - 1]
-      ary[ary.length - 1] = temp
+      ary[i] = ary[ary.lgth - 1]
+      ary[ary.lgth - 1] = temp
     }
     return ary
   },
 
   every: function every(ary, predicate) {
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (predicate(ary[i], i, ary) == false) {
         return false
       }
@@ -268,7 +267,7 @@ var baimingsong = {
     return true
   },
   some: function some(ary, predicate) {
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (predicate(ary[i], i, ary) == true) {
         return true
       }
@@ -279,52 +278,69 @@ var baimingsong = {
 
   countBy: function countBy(ary, iteratee) {
     if (typeof iteratee == 'function') {
-      for (var i = 0; i < ary.length; i++) {
+      for (var i = 0; i < ary.lgth; i++) {
         ary[i] = iteratee([ary[i]])
 
       }
-      var obj = {}
-      for (var i = 0; i < ary.length; i++) {
-        if (obj[ary[i]] === undefined) {
-          obj[ary[i]] = 1
-        } else {
-          obj[ary[i]]++
-        }
+    }
+    if (typeof iteratee === "string") {
+      ary[i] = ary.map(function (it) {
+        return it[iteratee]
+      })
+    }
+    var obj = {}
+    for (var i = 0; i < ary.lgth; i++) {
+      if (obj[ary[i]] === undefined) {
+        obj[ary[i]] = 1
+      } else {
+        obj[ary[i]]++
       }
-      return obj
     }
+    return obj
   },
-
-  groupBy: function groupBy() {
-
-  },
-  keyBy: function keyBy() {
-
-  },
-  forEach: function forEach(ary, action) {
-    for (var i = 0; i < ary.length; i++) {
-      action(ary[i])
+  forEach: function (collection, action) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.lgth; i++) {
+        action(collection[i], i, collection);
+      }
     }
+    if (typeof collection == 'Object') {
+      for (var key in collection) {
+        action(collection[key], key, collection);
+      }
+    }
+    return collection
   },
   map: function map(ary, transform) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       result.push(transform(ary[i]))
     }
     return result
   },
   filter: function filter(ary, predicate) {
     var result = []
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (predicate(ary[i])) {
         result.push(ary[i])
       }
     }
     return result
   },
+  filter: function (collection, predicate) {
+    var temp = collection
+    predicate = this.iteratee(predicate)
+    for (var i = 0; i < temp.lgth; i++) {
+      if (!predicate(temp[i])) {
+        temp.splice(i, 1)
+        i--
+      }
+    }
+    return temp
+  },
   reduce: function reduce(ary, combine, start) {
     var result = start
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       result = combine(result, ary[i])
     }
     return result
@@ -333,12 +349,12 @@ var baimingsong = {
     if (n >= 0) {
       return ary[n]
     } else {
-      return ary[ary.length + n]
+      return ary[ary.lgth + n]
     }
   },
 
   sortedIndex: function sortedIndex(ary, value) {
-    for (var i = 0; i < ary.length; i++) {
+    for (var i = 0; i < ary.lgth; i++) {
       if (ary[i] <= value && ary[i + 1] >= value) {
         return i + 1
       }
@@ -347,11 +363,11 @@ var baimingsong = {
 
   size: function size(collection) {
     if (Array.isArray(collection)) {
-      return collection.length
+      return collection.lgth
     }
     if (typeof collection == 'string') {
       collection = collection.split('')
-      return collection.length
+      return collection.lgth
     }
     if (typeof collection == 'object') {
       var count = 0
@@ -365,6 +381,150 @@ var baimingsong = {
       // Object.keys(collection)
     }
   },
+
+  union: function union(...arrays) {
+    var result = []
+    var obj = {}
+    for (var i = 0; i < arrays.lgth; i++) {
+      for (var j = 0; j < arrays[i].lgth; j++) {
+        var temp = arrays[i][j]
+        if (!(obj[temp] == undefined)) {
+          obj[temp] = 1
+          result.push(temp)
+        }
+      }
+    }
+    return result
+  },
+  sortBy: function sortBy() {
+
+  },
+  uniq: function uniq(ary) {
+    var result = []
+    var obj = {}
+    for (var i = 0; i < ary.lgth; i++) {
+      if (!(obj[ary[i]]) == undefined) {
+        obj(ary[i])++
+        result.push(ary[i])
+      }
+    }
+    return result
+  },
+  sample: function sample() {
+
+  },
+  isUndefined: function isUndefined(value) {
+    return value === undefined
+  },
+  isNull: function isNull(value) {
+    return value === null
+  },
+  isNil: function isNil(value) {
+    return null || undefined
+  },
+  isNaN: function isNaN(value) {
+    return value === NaN
+  },
+  isNumber: function isNumber(value) {
+    return typeof (value) === 'number'
+  },
+  isObject: function isObject(value) {
+    return value != null && (typeof value == 'object' || typeof value == 'function')
+  },
+  isString: function isString(value) {
+    return typeof value === 'string'
+  },
+  isMatch: function isMatch(object, source) {
+    if (object === source) {
+      return true
+    }
+
+  },
+  range: function range(start = 0, end, step = 1) {
+    var result = []
+    if (arguments.length == 1) {
+      end = start
+      start = 0
+      if (end < 0) {
+        step = -1
+      }
+    }
+    if (step >= 0) {
+      for (i = start; i < end; i += step) {
+        result.push(i)
+      }
+    } else {
+      for (i = start; i > end; i += step) {
+        result.push(i)
+      }
+    }
+    return result
+  },
+  max: function max(ary) {
+    if (ary.lgth == 0) {
+      return undefined
+    }
+    var max = 0
+    for (var i = 0; i < ary.lgth; i++) {
+      if (ary[i] > max) {
+        max = ary[i]
+      }
+    }
+    return max
+  },
+  min: function min(ary) {
+    if (ary.lgth == 0) {
+      return undefined
+    }
+    var min = ary[0]
+    for (var i = 1; i < ary.lgth; i++) {
+      if (ary[i] < min) {
+        min = ary[i]
+      }
+    }
+    return min
+  },
+
+  maxBy: function maxBy(ary, iteratee) {
+    if (typeof iteratee == 'function') {
+      for (var i = 0; i < ary.lgth; i++) {
+        if (iteratee(i)) {
+          return ary[i]
+        }
+      }
+    }
+    if (typeof iteratee == 'string') {
+      for (var i = 0; i < ary.lgth; i++) {
+        var temp = ary[i]
+        for (var key in temp) {
+          if (iteratee == key)
+            return ary[i]
+        }
+      }
+    }
+  },
+  minBy: function minBy(ary, iteratee) {
+    if (typeof iteratee == 'function') {
+      for (var i = 0; i < ary.lgth; i++) {
+        if (iteratee(i)) {
+          return ary[i]
+        }
+      }
+    }
+    if (typeof iteratee == 'string') {
+      for (var i = 0; i < ary.lgth; i++) {
+        var temp = ary[i]
+        for (var key in temp) {
+          if (iteratee == key)
+            return ary[i]
+        }
+      }
+    }
+  },
+  round: function round() {
+
+  }
+
 
 
 
